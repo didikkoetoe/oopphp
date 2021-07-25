47aafd9d11ffe0c78d6036831cdf8788686c82e7
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-25 22:09:23 +0700

    membuat method sederhana dan menggunakannya

--- episode3.php
+++ episode3.php
@@ -7,11 +7,26 @@ class Buku
         $pengarang = "pengarang",
         $penerbit = "penerbit",
         $tTerbit = 1980;
+    public function sayHallo()
+    {
+        return "Hello World!";
+    }
 }
 
 // Cara inisiasi class
-$produk1 = new Buku();
-$produk1->judul = "the 7 Habits of Highly Effective People";
-$produk1->pengarang = "Stepehen R Covey";
-$produk1->penerbit = "Covey Leadership Center";
-var_dump($produk1);
+// $produk1 = new Buku();
+// $produk1->judul = "the 7 Habits of Highly Effective People";
+// $produk1->pengarang = "Stepehen R Covey";
+// $produk1->penerbit = "Covey Leadership Center";
+// var_dump($produk1);
+// Di atas menampilkan object dengan var_dump
+
+// Menampilkan object dengan echo
+$buku1 = new Buku();
+$buku1->judul = "Atomic Habit";
+$buku1->pengarang = "James Clear";
+$buku1->penerbit = "tidak tahu";
+
+echo "judul : $buku1->judul ,<br>Pengarang : $buku1->pengarang";
+echo "<br>";
+echo $buku1->sayHallo();
