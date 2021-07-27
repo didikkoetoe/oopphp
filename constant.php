08b8d030db9eb48225bb4bfce512a4bdb4c0be6a
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-27 18:15:38 +0700

    Membuat file constant.php

---
+++ constant.php
@@ -0,0 +1,24 @@
+<?php
+
+// * Ada dua cara membuat constant
+// * define() dan const
+// define("NAMA", "Didik Prabowo");
+// ! nama constant sebaiknya menggunakan huruf besar semua
+// echo NAMA;
+
+// echo "<br>";
+// const UMUR = 19;
+// echo UMUR;
+
+// * define tidak bisa di gunakan di dalam class sedangkan const bisa
+// class Contoh
+// {
+//     public const NAMA = "Didik Prabowo";
+// }
+
+// echo Contoh::NAMA;
+
+// * Contoh magic constant
+
+echo __LINE__;
+echo __FILE__;
